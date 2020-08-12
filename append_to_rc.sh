38f274fb6f1c738276fbf1777c0f10dfac562224
prajeeshag <prajeeshag@gmail.com>
2020-08-12 23:02:16 +0530

    Updated

--- append_to_rc.sh
+++ append_to_rc.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-strng="source $BASEDIR/bashrc # dotfiles_appended"
 
-if [ -f $HOME/.bashrc ]; then
+if [ -f "$HOME/.bashrc" ]; then
+	strng="source $BASEDIR/bashrc # dotfiles_appended"
   if grep -q "dotfiles_appended" $HOME/.bashrc; then
-    sed -i "/dotfiles_appended/c\\$strng" $HOME/.bashrc
-		echo "appended to bashrc"
-  else
+		sed -i.bak '/dotfiles_appended/d' $HOME/.bashrc
     echo $strng >> $HOME/.bashrc
-		echo "added to bashrc"
+		echo "appended to bashrc"
   fi
-else
-  echo "No regular .bashrc file found in $HOME"
 fi
+
+if [ -f "$HOME/.zshrc" ]; then
+	strng="source $BASEDIR/zshrc # dotfiles_appended"
+  if grep -q "dotfiles_appended" $HOME/.zshrc; then
+		sed -i.bak '/dotfiles_appended/d' $HOME/.zshrc
+    echo $strng >> $HOME/.zshrc
+		echo "appended to zshrc"
+  fi
+	exit 0
+fi
+
+echo "No regular .bashrc or .zshrc file found in $HOME"
+exit 1
 
