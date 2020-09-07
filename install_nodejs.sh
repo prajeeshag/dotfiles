0dc35f5b5969e6ee1afe3d0214cfab7203579075
prajeesh <prajeesh@pop-os.localdomain>
2020-09-07 18:37:25 +0530

    Updated

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,7 +3,8 @@ set -e
 
 wget -c https://nodejs.org/dist/v12.18.3/node-v12.18.3-linux-x64.tar.xz -O node.tar.xz
 mkdir -p nodejs && tar xf node.tar.xz -C nodejs --strip-components 1
-dirnm="$(pwd)/nodejs/bin"
+mkdir -p "$HOME/.local"
+dirnm="$HOME/.local/nodejs/bin"
 strng="export PATH=$dirnm:\$PATH # nodejs_paths"
 sed -i.bak '/nodejs_path/d' "$HOME/.bashrc"
 echo $strng >> "$HOME/.bashrc"
