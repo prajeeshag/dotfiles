d5a99f836100bcd6d84870d2deec80737dcf3406
Prajeesh Ag <athippp@login4.hsn.shaheen.hpc.kaust.edu.sa>
2024-06-14 16:30:49 +0300

    Disabled <leader>+h keybinding for nvim

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+local nomap = vim.keymap.del
+nomap("n", "<leader>h")
