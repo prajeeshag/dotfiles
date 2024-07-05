64198163007cfd60a737b442ecfe6ef2548e2bec
Prajeesh <prajeesh.athippattagopinathan@kaust.edu.sa>
2024-07-05 13:27:04 +0300

    added cylc snippets

---
+++ dot_config/nvim/lua/snippets/cylc.lua
@@ -0,0 +1,17 @@
+return {
+	s(
+		"template",
+		t({
+			"#!Jinja2",
+			"",
+			"[task parameters]",
+			"",
+			"[scheduling]",
+			"   initial cycle point = ",
+			"   final cycle point = ",
+			"   [[graph]]",
+			"",
+			"[runtime]",
+		})
+	),
+}
